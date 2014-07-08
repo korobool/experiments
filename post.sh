61cc4bd57779eb07b6d894ee23d887b130fde1f4
Dmytro Novikov <dimetrios@gmail.com>
2014-07-08 23:41:05 +0300

    Add simple server.

---
+++ post.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -v -X POST -H "Content-Type: application/json" -d @in.json http://127.0.0.1:8080/leadinga
---
+++ simple_srv.py
@@ -0,0 +1,27 @@
+import asyncio
+import aiorest
+import json
+
+def leadinga(request):
+    try:
+        jo = request.json_body
+    except ValueError:    
+        return {'error': '10'}
+
+    words = [val for key, val in jo.items() if val.startswith('a')]
+
+    return json.JSONEncoder().encode({'words':words})
+    
+loop = asyncio.get_event_loop()
+srv = aiorest.RESTServer(hostname='127.0.0.1',loop=loop)
+
+srv.add_url('POST', '/leadinga', leadinga, True)
+server = loop.run_until_complete(loop.create_server(
+    srv.make_handler, '127.0.0.1', 8080))
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
