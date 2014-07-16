7037a883d732cece6083dc883502a8579e2226cd
Dmytro Novikov <dimetrios@gmail.com>
2014-07-16 15:16:29 +0300

    Fix exceptions handling.

--- restproxy/restproxy/restproxy.py
+++ restproxy/restproxy/restproxy.py
@@ -23,51 +23,49 @@ class RESTProxy(object):
     def _build_schema(self, sig):
         schema = {}
         pos = []
-        for name, param in sig.parameters.items():
-            if (param.kind == param.VAR_KEYWORD):
-                schema.update({'kwargs': {}})
-            elif (param.kind == param.VAR_POSITIONAL):
-                schema.update({'args': []})
-            else:
-                pos.append(name)
-
-        schema.update({'pos': pos})
+        if sig.parameters:
+            for name, param in sig.parameters.items():
+                if (param.kind == param.VAR_KEYWORD):
+                    schema.update({'kwargs': {}})
+                elif (param.kind == param.VAR_POSITIONAL):
+                    schema.update({'args': []})
+                else:
+                    pos.append(name)
+            schema.update({'pos': pos})
         return schema
 
     def _make_arguments(self, request, name, sig):
         try:
             args = []
             kwargs = {}
-            json = request.json_body
+            jo = request.json_body
             schema = self.schema[name]
 
-            if not json.keys() == schema.keys():
-                raise TypeError("Keys doesn't match schema.")
+            if not jo.keys() == schema.keys():
+                raise ValueError("Keys doesn't match schema")
 
-            for key, value in json.items():
-                if (key == 'args' and not isinstance(value, list)):
-                    raise TypeError("Wrong type for *args.")
-                if (key == 'kwargs' and not isinstance(value, dict)):
-                    raise TypeError("Wrong type for **kwargs.")
-                if (key == 'pos' and not isinstance(value, list)):
-                    raise TypeError("Wrong type for positional.")
-                if (key == 'pos' and not len(value) == len(schema[key])):
-                    raise TypeError("Positional arguments missmatch.")
+            if jo:
+                for key, value in jo.items():
+                    if (key == 'args' and not isinstance(value, list)):
+                        raise ValueError("Wrong type for *args")
+                    if (key == 'kwargs' and not isinstance(value, dict)):
+                        raise ValueError("Wrong type for **kwargs")
+                    if (key == 'pos' and not isinstance(value, list)):
+                        raise ValueError("Wrong type for positional")
+                    if (key == 'pos' and not len(value) == len(schema[key])):
+                        raise ValueError("Positional arguments missmatch")
 
-            args.extend(json['pos'])
-            args.extend(json['args'])
-            kwargs = json['kwargs']
+                args.extend(jo['pos'])
+                args.extend(jo['args'])
+                kwargs = jo['kwargs']
+            
             ba = sig.bind(*args, **kwargs)
-
-        except ValueError:
-            raise
-        except TypeError:
-            raise
-
+        except ValueError as e:
+            raise aiorest.RESTError(400, "Bad Request", {'msg': str(e)})
         return ba
 
     def _wrap_response(self, response):
-        return json.dumps(response, default=lambda o: o.__dict__)
+        return {'response':response}
 
     def _build_routes(self):
         for name, method in self._methods.items():
@@ -78,14 +76,9 @@ class RESTProxy(object):
                 method = self._methods[name]
                 sig = signature(self._methods[name])
                 ba = self._make_arguments(request, name, sig)
-                if ba:
-                    return self._wrap_response(method(*ba.args, **ba.kwargs))
-                return {}
+                return self._wrap_response(method(*ba.args, **ba.kwargs))
 
-            if sig.parameters:
-                self.srv.add_url('POST', '/' + name, fn, True)
-            else:
-                self.srv.add_url('GET', '/' + name, fn, False)
+            self.srv.add_url('POST', '/' + name, fn, True)
 
         def api():
             return self._wrap_response(self.schema)
